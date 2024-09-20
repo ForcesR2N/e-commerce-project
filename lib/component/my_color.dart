5d7f5f9e7a0cfa3d11e6986d7f408c203d220f2a
ForcesR2N <muhammadzuhrizal07@gmail.com>
2024-09-20 22:41:08 +0700

    ui homepage

---
+++ lib/component/my_color.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color fontAddNumber = Color(0xFF606060);
+  static const Color buttonGreen = Color(0xFF006D0A);
+  static const Color backgroundColor = Color(0xFFF0F3BD);
+  static const Color textColor = Color(0xFF333333);
+  static const Color bgNomorHp = Color(0xFFE3E2E2);
+  static const Color primarycolor = Color(0xFFFF782B);
+}
